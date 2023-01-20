24a529069e0f34ea02ec7b66cb25af592a8bece4
Murilo Santori Boff <murilosantori45@gmail.com>
2023-01-20 15:14:41 -0300

    Finalização da pagina de cadastro

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 def cadastrar(request):
     if request.method == 'GET':
-        return render(request, 'cadastrar.html')
+        status = request.GET.get('status')
+        return render(request, 'cadastrar.html', {'status':status})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        if len(nome.strip()) == 0 or len(email.strip()) == 0:
+            messages.add_message(request, messages.constants.ERROR, 'Nome ou E-mail invalidos')
+            return redirect('/auth/cadastrar/')
 
+        if len(senha.strip()) < 8:
+            messages.add_message(request, messages.constants.ERROR, 'Senha invalida ou muito curta')
+            return redirect('/auth/cadastrar/')
+
+        else:
+            try:
+                user = User.objects.create_user(username=nome, email=email, password=senha)
+                user.save()
+                messages.add_message(request, messages.constants.SUCCESS, 'Conta criada, agora faça LOGIN')
+                return redirect('/auth/cadastrar/')
+            except:
+                messages.add_message(request, messages.constants.ERROR, 'Erro na hora de cadastrar')
+                return redirect('/auth/cadastrar/?status=3')
 def login(request):
     return HttpResponse('Login')
