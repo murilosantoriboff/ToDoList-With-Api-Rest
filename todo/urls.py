f9c37434d4c99e870dd1a7acb13a09cea5f0e3ee
Murilo Santori Boff <murilosantori45@gmail.com>
2023-01-26 10:28:23 -0300

    Exibir as Tarefas e começar a crias as mesmas

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home') 
+    path('home/', views.home, name='home'),
+    path('criar_tarefa/', views.criar_tarefa, name='criar_tarefa')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Tarefa
 
 @login_required(login_url='/auth/login/')
 def home(request):
-    # Mandar para home os dados da model Tarefa
-    return render(request, 'home.html')
+    tarefas = Tarefa.objects.all()
+    return render(request, 'home.html', {'tarefas':tarefas})
+
+@login_required(login_url='/auth/login/')
+def criar_tarefa(request):
+    if request.method == 'GET':
+        return render(request, 'criar_tarefa.html')
+    #elif request.method == 'POST':
+    #    pass
